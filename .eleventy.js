0f48a96424f0b1b808aa64049c2277933a466318
Vladimir <vlasad@gmail.com>
2020-12-25 12:09:56 +0300

    refactoring default template engines

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/**/*");
 
   return {
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
     dir: {
       input: "src"
     }
