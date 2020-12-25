ce3f56cbbb61279377ca5be30102e1809f1c98e6
Vladimir <vlasad@gmail.com>
2020-12-25 15:21:23 +0300

    add redirects

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/favicon.ico");
   eleventyConfig.addPassthroughCopy("src/assets/**/*");
+  eleventyConfig.addPassthroughCopy("_redirects");
 
   return {
     markdownTemplateEngine: "njk",
